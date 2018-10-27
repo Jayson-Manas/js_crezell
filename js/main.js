34e3169f1cf695a5a23111ad2d1d96320e190a99
Jayson-Manas <jaysonmanas@gmail.com>
2018-10-28 01:38:45 +0200

    fixed input error

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ function makeNegative (number) {
     else{
         number=1*number
     }
-console.log(number)
+        console.log(number)
     // write the code for your function here
     
   
@@ -47,7 +47,8 @@ console.log(number)
   // call your function here
   for(var i=0; i<3; i++) {
     var y =num[i]
-    double(y)
+    double(y)}
+
   // 3. You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value.
   // Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not.
   
